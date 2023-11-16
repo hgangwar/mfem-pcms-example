e939fb09183849f163682389b4c6683c85afe89f
Fuad Hasibul Hasan <fuadhhasan.acc@gmail.com>
2023-11-15 21:33:54 -0500

    initial commit: with all tools

---
+++ config.sh
@@ -0,0 +1,8 @@
+export NVCC_WRAPPER_DEFAULT_COMPILER=`which mpicxx`
+cmake -S . -B build \
+	-DCMAKE_CXX_COMPILER=`which mpicxx` \
+	-DCMAKE_C_COMPILER=`which mpicc` \
+	-DMFEM_ROOT=/lore/hasanm4/mFEM/install/mfem/ \
+	-Dpcms_ROOT=/lore/hasanm4/pcms-coupler/build/PASCAL61/pcms/install \
+	-Dperfstubs_DIR=/lore/mersoj/laces-software/build/perfstubs/install/lib64/cmake/
+#	-Dperfstubs_ROOT=/lore/mersoj/laces-software/build/perfstubs/install
